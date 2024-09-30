3e800ed83d9f9eb3a6f2e007c62c8aab1c2780ad
Misha Smishnyi <mishasmish@gmail.com>
2024-09-30 10:43:15 +0300

    Добавьте файлы проекта.

---
+++ lab3task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>  
+
+int main() {
+    //const
+    const double a = 5;
+    const double b = -4;
+    const double c = 1;
+
+    //calc
+    double chisel = a + tan(b); // a + tan(b)                   
+    double znam = 3 * a - 4 * sqrt(a + c) + 3 * fabs(b);  // 3a - 4sqrt(a + c) + 3|b|
+
+    double f = chisel / znam;
+
+    //output
+    std::cout << "Function 'F' = " << f << std::endl;
+
+    return 0;
+}
